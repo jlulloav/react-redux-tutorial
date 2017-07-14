e0f020e8eeb95fb7f99981cb02069169977e86f3
Norbyn <norbyn.leyva@towersoft.net>
2017-07-14 11:51:57 -0400

    Training with react-redux

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import Footer from './Footer'
 import AddTodo from '../containers/AddTodo'
 import VisibleTodoList from '../containers/VisibleTodoList'
+import UndoRedo from '../containers/UndoRedo'
 
 const App = () => (
     <div>
         <AddTodo />
         <VisibleTodoList />
+        <Footer />
+        <UndoRedo />
     </div>
 )
 
